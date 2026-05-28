4620525c529d61555e4564f419544969fde54560
SrestMilan <shrestham780@gmail.com>
2026-05-28 14:27:23 +0100

    add library

--- batterNotify.py
+++ batterNotify.py
@@ -1,1 +1,18 @@
-#file added
+""""
+import psutil  # retrieve battery notification
+from plyer import notification # shows battery notification
+import time
+
+lowBattery=15     # low battery level
+fullBattery=100   # full battery level
+checkInterval=60  # check battery every 60 seconds
+"""
+from plyer import notification
+
+notification.notify(
+    title="Test",
+    message="Plyer is working",
+    app_name="MyApp",
+    timeout=10
+)
+
